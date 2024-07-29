75ebcb4acef8feaf38cebe36c2e21dc62c8a71d5
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-07-29 18:13:22 +0530

    Problem 1

---
+++ Chapter 1/02_problem.cpp
@@ -0,0 +1,14 @@
+
+//TODO: Area Of Circle
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    float r = 4.3;
+    float area = 3.14 * r * r;
+    cout << "Area of the circle with radius " << r << " is: " << area << endl;
+
+return 0;
+}
