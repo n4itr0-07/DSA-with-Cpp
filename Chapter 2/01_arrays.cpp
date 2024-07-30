06e727d999bdb213481757debdbe909f38f79aac
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-07-30 18:26:12 +0530

    Arrays In C++

---
+++ Chapter 2/01_arrays.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Arrays
+int main () {
+// int arr[3];
+
+// cin >> arr[0] >> arr[1] >> arr[2];
+// cout << arr[0];
+
+// 2D array
+
+int arr[2][3]; // TODO: Here 2 is Row and 3 is Column
+
+    arr[1][2] = 78; // TODO:  Corrected to access the last element of the second row
+    
+    std::cout << arr[1][2] << std::endl; // TODO:  Output the value stored in arr[1][2]
+
+return 0;
+}
