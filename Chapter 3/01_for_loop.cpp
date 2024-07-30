796f74fd161980395a36743466063c29e90d1473
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-07-30 19:16:45 +0530

    Loops In C++ Code

---
+++ Chapter 3/01_for_loop.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+// cout << "Salik Seraj Naik" << endl;
+// cout << "Salik Seraj Naik" << endl;
+// cout << "Salik Seraj Naik" << endl;
+// cout << "Salik Seraj Naik" << endl;
+// cout << "Salik Seraj Naik" << endl;
+// cout << "Salik Seraj Naik" << endl;       // This wiil take to much time 
+// cout << "Salik Seraj Naik" << endl;
+// cout << "Salik Seraj Naik" << endl;
+// cout << "Salik Seraj Naik" << endl;
+// cout << "Salik Seraj Naik" << endl;
+
+
+// TODO: For Loop
+//    for(int i =1; i <= 50; i++) {        
+
+//       cout << "Salik Seraj Naik " << i << endl;
+// }
+
+// TODO: While Loop
+int i = 1;
+while(i <= 5) {        
+    cout << "Salik Seraj Naik " << i << endl;
+    i++;
+}
+
+
+    return 0;
+}
