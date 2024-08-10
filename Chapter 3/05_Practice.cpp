2bd544d8bbb5674e328f6ef2acdad2f34f7d40ef
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-08-10 20:52:40 +0530

    Practice On Loops

---
+++ Chapter 3/05_Practice.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number of times you want to print 'Hello World!': ";
+    cin >> n;
+for(int i = 1; i <= n; i++) {
+    cout << " Hello World ! " << i << endl;
+}
+
+
+    return 0;
+}
