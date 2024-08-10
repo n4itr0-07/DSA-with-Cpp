7b3543d085fd780383039c8a6e0750909a94b3e8
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-08-10 21:01:46 +0530

    Practice On Loops

---
+++ Chapter 3/06_Practice.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+int num;
+cout << "Enter number : ";
+
+cin >> num;
+
+for(int i = 1; i <= num; i++)
+cout << i <<  endl;
+
+
+    return 0;
+}
