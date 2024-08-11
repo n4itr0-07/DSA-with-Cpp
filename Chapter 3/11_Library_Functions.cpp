3206e46fb6dd139a328210d4023e2cbc156532f9
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-08-11 15:57:42 +0530

    Basic library In C++

---
+++ Chapter 3/11_Library_Functions.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+// cout << sqrt(100); // Square root of 100
+ cout << "Square root of 100 is: " << sqrt(100) << endl;
+
+ cout << endl;
+
+//  cout << min(10,11); // Minimum
+  cout << "Minimum of 10 and 11 is: " << min(10,11) << endl;
+
+  cout << endl;
+
+//  cout << max(10,11); // Maximum
+  cout << "Maximum of 10 and 11 is: " << max(10,11) << endl;
+
+  cout << endl;
+
+// cout << pow(2,4);  // Power of 2
+  cout << "2 raised to the power of 4 is: " << pow(2,4) << endl;
+
+  cout << endl;
+
+    return 0;
+}
