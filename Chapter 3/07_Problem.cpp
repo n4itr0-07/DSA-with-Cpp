61ca747c8381f1e9906255fd8946fec11064f57d
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-08-11 14:30:38 +0530

    Question Problem

---
+++ Chapter 3/07_Problem.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int n;
+
+
+    // Check even number from 1 to 100
+    for(int i=1; i<=100; i++){
+        if(i % 2 == 0){
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+}
