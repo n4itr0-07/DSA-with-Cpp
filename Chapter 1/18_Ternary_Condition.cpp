dcd27b294cc74cd98e5801e45000273c1fdda52b
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-09-06 11:11:43 +0530

    Ternary Statement In Conditional Statement

---
+++ Chapter 1/18_Ternary_Condition.cpp
@@ -0,0 +1,21 @@
+
+//TODO: Ternary
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int n; // variable to store user input
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    // Ternary operator
+    cout << "The number is " << (n >= 0? "Positive" : "Negative") << endl;
+
+
+
+    return 0;
+}
