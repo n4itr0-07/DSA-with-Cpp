a95e441baa49f8ab0ada4df248154974df17cb6c
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-09-06 12:20:17 +0530

    Loops Revision

---
+++ Chapter 3/Loops_study.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//TODO: This is While Loop
+int main() {
+    // int n;
+    // cout << "Enter a positive integer: ";
+    // cin >> n;
+
+    // // Start from 1 and go up to the number provided by the user
+    // int i = 1; // Start from 1
+    // while (i <= n) {
+    //     cout << i << " ";  // Print the current number
+    //     i++;  // Increment the number
+    // }
+
+ //TODO: This is For Loop
+
+ /*
+ for(initialisation;condition;updation) {
+        work
+ */   
+
+    // int count;
+    // cout << "Enter a positive integer: ";
+    // cin >> count;
+
+    // int i = 1;
+    // for(int i = 1; i <= count; i++) {
+    //     cout << i << " ";  // Print the current number
+    // }
+
+    //FIXME: Q:- Sum of numbers from 1 to n
+
+    // int n;
+    // cout << "Enter a positive integer: ";
+    // cin >> n;
+
+    // int sum = 0;
+    // for(int i = 1; i <= n; i++) {
+    //     sum += i;  // Add the current number to the sum
+    // }
+
+    // cout << "The sum of numbers from 1 to " << n << " is: " << sum << endl;  // Print the result
+
+    //FIXME: Q2:- Sum of all odd numbers from 1 to n
+
+    // int input;
+    // cout << "Enter a positive integer: ";
+    // cin >> input;
+    
+    // int sum = 0;
+    // for(int i = 1; i <= input; i++) {
+    //     if(i % 2!= 0) {  // Check if the number is odd
+    //         sum += i;  // Add the odd number to the sum
+    //     }
+    // }
+    
+    // cout << "The sum of all odd numbers from 1 to " << input << " is: " << sum << endl;  // Print the result
+
+    //TODO: Do While Loop
+
+    // int n;
+    // int i = 1;
+    // cout << "Enter a positive integer: ";
+    // cin >> n;
+
+    // do{
+    //     cout << i << " ";
+    //     i++;
+
+    // } while(i <= n);
+
+    return 0;
+}
