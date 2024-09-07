f15376b44316c98a55e6a4aa5160a2ff4c0362e5
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-09-07 10:49:36 +0530

    Pattern Problem 5 | Inverted Triangle Pattern

--- Chapter 2/Pattern_Question_19.cpp
+++ Chapter 2/Pattern_Question_19.cpp
@@ -21,3 +21,25 @@ int main() {
     }
     return 0;
 }
+
+// #include <stdio.h>
+// #include <conio.h>
+
+// int main() {
+
+//     int n = 4;
+//     int num = 1;
+    
+//     for(int i = 0; i < n; i++) {
+//         for(int j = 0; j < i; j++) {
+//             printf(" ");
+//         }
+        
+//         for(int j = 0; j < n - i; j++) {
+//             printf("%d", i + 1);
+
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
