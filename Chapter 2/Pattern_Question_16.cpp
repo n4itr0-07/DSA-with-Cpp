882849e61c267591c5acece1f961efb17bb8900c
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-09-07 09:49:25 +0530

    Pattern Problem 2 by Apna College

--- Chapter 2/Pattern_Question_16.cpp
+++ Chapter 2/Pattern_Question_16.cpp
@@ -4,17 +4,29 @@
 
 
 int main () {
-    int n = 3;
-    // cout << "Enter the number of elements : ";
-    // cin >> n;
-    int num = 1;
+//TODO: Number Pattern
+    // int n = 3;
+    // // cout << "Enter the number of elements : ";
+    // // cin >> n;
+    // int num = 1;
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            cout << num << " ";
-            num++;
+    // for(int i = 0; i < n; i++) {
+    //     for(int j = 0; j < n; j++) {
+    //         cout << num << " ";
+    //         num++;
+    //     }
+    //    cout << endl;
+    // }
+//TODO: Alphabet Pattern
+    int n = 3;  // Size of the matrix
+    char ch = 'A';  // Initialize ch only once before the loops
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << ch << " ";  // Print the current character
+            ch++;  // Increment the character to the next letter
         }
-       cout << endl;
+        cout << endl;  // Move to the next line after each row
     }
     return 0;
 }
