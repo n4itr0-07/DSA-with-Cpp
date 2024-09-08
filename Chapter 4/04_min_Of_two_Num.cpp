4cede5d5eb519eb5ae1a8584ef323504b854f45a
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-09-08 12:26:04 +0530

    Minimum Of Two Numbers Using Functions

--- Chapter 4/04_min_Of_two_Num.cpp
+++ Chapter 4/04_min_Of_two_Num.cpp
@@ -16,7 +16,27 @@ int minOfTwo() {
     }
 }
 int main() {
-cout << "Min Of Two  Numbers Is : " << minOfTwo() << endl;
-return 0;
+    cout << "Min Of Two  Numbers Is : " << minOfTwo() << endl;
+    return 0;
 
 }
+
+#include <iostream>
+using namespace std;
+
+
+// void evenOdd() {
+//     int num;
+//     cout << "Enter a number: ";
+//     cin >> num;
+
+//     if(num % 2 == 0){
+//         cout << num << " is an even number." << endl;
+//     } else {
+//         cout << num << " is an odd number." << endl;
+//     }
+// }
+// int main () {
+//     evenOdd();
+//     return 0;
+// } //TODO: First Method
