77992f15b2e04b98ddaac74c23b8797c740f35d5
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-09-12 10:35:17 +0530

    Chapter 06 | Home Work

--- Chapter 06/06_Home_Work.cpp
+++ Chapter 06/06_Home_Work.cpp
@@ -10,17 +10,17 @@ int main() {
     int size = 6;
 
     int smallest = INT_MAX;
-   // int index = -1; // Variable to store the index of the smallest element
+    int index = -1; // Variable to store the index of the smallest element
 
     for(int i = 0; i < size; i++) {
         if(nums[i] < smallest) {
             smallest = nums[i];
-          //  index = i; // Update the index when a new smallest element is found
+            index = i; // Update the index when a new smallest element is found
         }
         // smallest = min(nums[i], smallest); | this is also a method in place of if statement
     }
 
     cout << "SMallest = " << smallest << endl;
-   // cout << "Index of -15 is : " << index << endl;
+    cout << "Index of -15 is : " << index << endl;
     return 0;
 }
