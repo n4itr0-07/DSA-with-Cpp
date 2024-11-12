9f335bf787f3468111ec74be444f9505dc2feff3
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-11-12 11:26:33 +0530

    C++ Playlist | Lecture 2 | Variables & Constant

---
+++ C++ Playlist/Lecture 2/02_varconst.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int score;
+    score = 110;
+
+    int balance = 99;
+
+    // For making unique key or a variable that cannot be be changed
+    const int uniqueKey = 123456789;
+    // Now if we try to change the variable valeue then:
+    // uniqueKey = 987654321; GIve Error
+
+    cout << "Welcome to 127.0.0.1" << endl;
+    // This is bakwas comment
+    cout << "Welcome to 127.0.0.1" << endl;
+
+    return 0;
+}
