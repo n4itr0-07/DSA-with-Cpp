6c26ec155416e9d6bc68b390cc5263ea2a74b2e2
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-11-20 18:53:36 +0530

    C++ Playlist | Lecture 3 | Primitivies Data Types

---
+++ C++ Playlist/Lecture 3/03_primitivies_data.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int teaLeaves = 50;
+    float waterTemperature = 85.5;
+    double priceOfTea = 299.99;
+    char teaGrade = 'A';
+    bool isTeaReady = false;
+
+    cout << isTeaReady << endl;
+
+    return 0;
+}
